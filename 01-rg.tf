4c7f26dcaac7fc7aa1cff88321d1e81766aa73c0
Oscar Pettersson <pson93@gmail.com>
2024-11-11 09:40:56 +0100

    working on key vault for sens obj

---
+++ 01-rg.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rg-project" {
+  name     = "rg-project"
+  location = "North Europe"
+}
+
---
+++ 02-variables.tf
@@ -0,0 +1,5 @@
+variable "subscription-id-key-vault" {
+    type = string
+    sensitive = true
+    description = "subscription-id"
+}
