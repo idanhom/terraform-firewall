80c2f2857eced3f3a445906d3fe7502535d0097d
Oscar Pettersson <pson93@gmail.com>
2024-11-11 15:28:58 +0100

    newest version

---
+++ 03-firewall.tf
@@ -0,0 +1,13 @@
+resource "azurerm_firewall" "firewall" {
+  name                = "firewall"
+  location            = azurerm_resource_group.rg-project.location
+  resource_group_name = azurerm_resource_group.rg-project.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.firewall-subnet.id
+    public_ip_address_id = azurerm_public_ip.firewall-ip.id
+  }
+}
