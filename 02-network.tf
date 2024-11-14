7564563ddbb507b80be95268acb1d2a43e4d8f23
Oscar Pettersson <pson93@gmail.com>
2024-11-14 10:52:33 +0100

    added lifecycle argument create_before_destroy to handle problems with disassociate on destruction

--- 02-network.tf
+++ 02-network.tf
@@ -18,4 +18,7 @@ resource "azurerm_public_ip" "firewall_ip" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard" //public ip 
+  lifecycle {
+    create_before_destroy = true
+  }
 }
