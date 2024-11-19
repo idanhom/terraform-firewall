5575a2be9448f7de00b2455e162b1d2c3565fa58
Oscar Pettersson <pson93@gmail.com>
2024-11-19 11:18:14 +0100

    vnet and subnet creation working

--- modules/azure_network/main.tf
+++ modules/azure_network/main.tf
@@ -13,13 +13,20 @@ resource "azurerm_subnet" "my_subnet" {
   name                 = each.value
   virtual_network_name = var.vnet_name
   address_prefixes     = [zipmap(var.subnet_name, var.subnet_address_prefix)[each.value]]
+
+  depends_on = [ azurerm_virtual_network.my_vnet ]
 }
+
+
+
 
 resource "azurerm_subnet" "firewall_subnet" {
   name                 = var.firewall_subnet_name //firewall subnet should always have the name "AzureFirewallSubnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = var.firewall_subnet_prefix
+
+  depends_on = [ azurerm_virtual_network.my_vnet ]
 }
 
 //more subnet... for_each relevant to vms?
