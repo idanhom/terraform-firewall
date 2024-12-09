1e26fe0f323ac4cbf94573af6aedfae864a0dce8
Oscar Pettersson <pson93@gmail.com>
2024-12-09 10:17:13 +0100

    removed more old code

--- modules/azure_firewall/main.tf
+++ modules/azure_firewall/main.tf
@@ -2,32 +2,31 @@
 # https://chatgpt.com/g/g-pDLabuKvD-terraform-guide/c/67489a47-2648-800b-99b3-d8fdc7becfc7
 
 
-# note: implement the afw map variable to make it like var.vnets (no for_each though...)
 
 resource "azurerm_virtual_network" "firewall_vnet" {
 
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  name          = var.afw.firewall_vnet_name   #var.firewall_vnet_name
-  address_space = var.afw.firewall_vnet_prefix #var.firewall_vnet_prefix
+  name          = var.afw.firewall_vnet_name   
+  address_space = var.afw.firewall_vnet_prefix
 }
 
 resource "azurerm_subnet" "firewall_subnet" {
 
   resource_group_name = var.resource_group_name
 
-  name                 = var.afw.firewall_subnet_name #var.firewall_subnet_name
-  virtual_network_name = var.afw.firewall_vnet_name   #var.firewall_vnet_name
+  name                 = var.afw.firewall_subnet_name
+  virtual_network_name = var.afw.firewall_vnet_name   
 
-  address_prefixes = var.afw.firewall_subnet_prefix #var.firewall_subnet_prefix
+  address_prefixes = var.afw.firewall_subnet_prefix 
 
   depends_on = [azurerm_virtual_network.firewall_vnet]
 }
 
 resource "azurerm_public_ip" "firewall_ip" {
 
-  name                = var.afw.firewall_ip_name #var.firewall_ip_name
+  name                = var.afw.firewall_ip_name 
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
@@ -38,7 +37,7 @@ resource "azurerm_public_ip" "firewall_ip" {
 }
 
 resource "azurerm_firewall" "firewall" {
-  name                = var.afw.firewall_name #var.firewall_name
+  name                = var.afw.firewall_name 
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name            = "AZFW_VNet"
