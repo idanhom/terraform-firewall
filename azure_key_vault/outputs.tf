f0b7b49b5f5b733b10429b48bdd863ad863da67c
Oscar Pettersson <pson93@gmail.com>
2025-01-08 17:13:05 +0100

    trying to solve key vault implementation

--- azure_key_vault/outputs.tf
+++ azure_key_vault/outputs.tf
@@ -3,10 +3,12 @@ output "key_vault_id" {
   value = azurerm_key_vault.key_vault.id
 }
 
-output "admin_username_secret_name" {
-  value = azurerm_key_vault_secret.admin_username.name
+output "admin_username_secret_value" {
+  value = azurerm_key_vault_secret.admin_username.value
+  sensitive = true
 }
 
-output "admin_password_secret_name" {
-  value = azurerm_key_vault_secret.admin_password.name
+output "admin_password_secret_value" {
+  value = azurerm_key_vault_secret.admin_password.value
+  sensitive = true
 }
