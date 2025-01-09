eddb7a36f043cca92e0f001b9ec71e2466f52766
Oscar Pettersson <pson93@gmail.com>
2025-01-09 13:17:35 +0100

    changed file path to install custom_data

--- modules/azure_compute/main.tf
+++ modules/azure_compute/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_linux_virtual_machine" "my_vms" {
 
 
 
-  custom_data = file("./custom_data/nginx-install.base64")
+  custom_data = file("./modules/azure_compute/custom_data/nginx-install.base64")
 
 
 
