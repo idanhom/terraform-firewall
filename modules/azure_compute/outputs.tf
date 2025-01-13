01b7808ad46e25f63cf361f9ec3cfe03a0a02346
Oscar Pettersson <pson93@gmail.com>
2025-01-13 13:58:33 +0100

    pushing cicd

--- modules/azure_compute/outputs.tf
+++ modules/azure_compute/outputs.tf
@@ -1,7 +1,6 @@
 output "vm_private_ip" {
   description = "map of private ip for vms"
   value       = { for vnet_name, vm in azurerm_linux_virtual_machine.my_vms : vnet_name => vm.private_ip_address }
-  sensitive   = false
 }
 
 output "vm_public_ip" {
