7cc1ebc9647ab4dcbc99cfcf79c429e4d0eeccde
Oscar Pettersson <pson93@gmail.com>
2025-01-14 09:06:42 +0100

    pushing pipe

--- modules/azure_storage_account/main.tf
+++ modules/azure_storage_account/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_private_dns_zone" "blob_dns_zone" {
   name                = "privatelink.blob.core.windows.net"
   resource_group_name = var.resource_group_name
 }
-
+#
 resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
   for_each = var.vnet_ids
 
