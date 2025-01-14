d771d2c5d3f740cdc8b12805647ee457fcf6fa9b
Oscar Pettersson <pson93@gmail.com>
2025-01-14 14:04:26 +0100

    pushing the pipe

--- modules/azure_storage_account/main.tf
+++ modules/azure_storage_account/main.tf
@@ -14,144 +14,144 @@ locals {
 }
 
 
-# resource "azurerm_private_dns_zone" "blob_dns_zone" {
-#   name                = "privatelink.blob.core.windows.net"
-#   resource_group_name = var.resource_group_name
-# }
-# #
-# resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
-#   for_each = var.vnet_ids
+resource "azurerm_private_dns_zone" "blob_dns_zone" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.resource_group_name
+}
 
-#   name                  = "${each.key}-blob-dns-link"
-#   resource_group_name   = var.resource_group_name
-#   private_dns_zone_name = azurerm_private_dns_zone.blob_dns_zone.name
-#   virtual_network_id    = each.value
-# }
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
+  for_each = var.vnet_ids
 
-# resource "azurerm_private_endpoint" "blob_private_endpoint" {
-#   for_each = var.subnet_ids
+  name                  = "${each.key}-blob-dns-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.blob_dns_zone.name
+  virtual_network_id    = each.value
+}
 
-#   name                = "${each.key}-blob-endpoint"
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   subnet_id           = each.value
+resource "azurerm_private_endpoint" "blob_private_endpoint" {
+  for_each = var.subnet_ids
 
-#   private_service_connection {
-#     name                           = "${each.key}-blob-endpoint"
-#     private_connection_resource_id = azurerm_storage_account.blob_storage_account.id
-#     is_manual_connection           = false
-#     subresource_names              = ["blob"]
-#   }
-#   depends_on = [azurerm_storage_account.blob_storage_account]
-# }
+  name                = "${each.key}-blob-endpoint"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = each.value
 
-
-# resource "azurerm_storage_account" "blob_storage_account" {
-#   name                            = "examplestoraccount5421"
-#   resource_group_name             = var.resource_group_name
-#   location                        = var.location
-#   account_tier                    = "Standard"
-#   account_replication_type        = "LRS"
-#   account_kind                    = "StorageV2"
-#   access_tier                     = "Cool"
-#   public_network_access_enabled   = true //Note: this should be "false" to disable public access?
-#   default_to_oauth_authentication = true
-
-#   allow_nested_items_to_be_public = false
-#   https_traffic_only_enabled      = true
-#   large_file_share_enabled        = true
-#   shared_access_key_enabled       = true
-
-#   blob_properties {
-#     container_delete_retention_policy {
-#       days = 7
-#     }
-
-#     delete_retention_policy {
-#       days                     = 7
-#       permanent_delete_enabled = false
-#     }
-#   }
-
-#   network_rules {
-#     default_action = "Deny" //note, this should be "Deny" to block public access?
-#     ip_rules = [var.runner_public_ip]
-#     bypass = ["AzureServices"]
-#     /*     private_link_access {
-#       endpoint_resource_id = azurerm_private_endpoint.example.id
-#     } */
-#   }
-
-#   share_properties {
-#     retention_policy {
-#       days = 7
-#     }
-#   }
-# }
+  private_service_connection {
+    name                           = "${each.key}-blob-endpoint"
+    private_connection_resource_id = azurerm_storage_account.blob_storage_account.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+  depends_on = [azurerm_storage_account.blob_storage_account]
+}
 
 
-# resource "azurerm_storage_container" "script_container" {
-#   name                  = "scripts"
-#   storage_account_id    = azurerm_storage_account.blob_storage_account.id
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_account" "blob_storage_account" {
+  name                            = "examplestoraccount5421"
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+  access_tier                     = "Cool"
+  public_network_access_enabled   = true //Note: this should be "false" to disable public access?
+  default_to_oauth_authentication = true
+
+  allow_nested_items_to_be_public = false
+  https_traffic_only_enabled      = true
+  large_file_share_enabled        = true
+  shared_access_key_enabled       = true
+
+  blob_properties {
+    container_delete_retention_policy {
+      days = 7
+    }
+
+    delete_retention_policy {
+      days                     = 7
+      permanent_delete_enabled = false
+    }
+  }
+
+  network_rules {
+    default_action = "Allow" //note, this should be "Deny" to block public access?
+    ip_rules = [var.runner_public_ip]
+    bypass = ["AzureServices"]
+    /*     private_link_access {
+      endpoint_resource_id = azurerm_private_endpoint.example.id
+    } */
+  }
+
+  share_properties {
+    retention_policy {
+      days = 7
+    }
+  }
+}
 
 
-# resource "azurerm_storage_blob" "script_blob" {
-#   name                   = "script.sh"
-#   storage_account_name   = azurerm_storage_account.blob_storage_account.name
-#   storage_container_name = azurerm_storage_container.script_container.name
-#   type                   = "Block"
-#   source                 = "${path.module}/custom_data/docker.sh" # Path to your local file
-
-#   depends_on = [azurerm_storage_container.script_container]
-# }
-
-# # 
+resource "azurerm_storage_container" "script_container" {
+  name                  = "scripts"
+  storage_account_id    = azurerm_storage_account.blob_storage_account.id
+  container_access_type = "private"
+}
 
 
+resource "azurerm_storage_blob" "script_blob" {
+  name                   = "script.sh"
+  storage_account_name   = azurerm_storage_account.blob_storage_account.name
+  storage_container_name = azurerm_storage_container.script_container.name
+  type                   = "Block"
+  source                 = "${path.module}/custom_data/docker.sh" # Path to your local file
+
+  depends_on = [azurerm_storage_container.script_container]
+}
+
+# 
 
 
 
-# data "azurerm_storage_account_sas" "scripts_sas" {
-#   connection_string = azurerm_storage_account.blob_storage_account.primary_connection_string
-#   https_only        = true
-#   signed_version    = "2022-11-02"
 
-#   # Define the resource types
-#   resource_types {
-#     service   = true
-#     container = true
-#     object    = true
-#   }
 
-#   # Define the storage account services
-#   services {
-#     blob  = true
-#     queue = false
-#     table = false
-#     file  = false
-#   }
+data "azurerm_storage_account_sas" "scripts_sas" {
+  connection_string = azurerm_storage_account.blob_storage_account.primary_connection_string
+  https_only        = true
+  signed_version    = "2022-11-02"
 
-#   # Use dynamic time for start and expiry
-#   start  = timestamp()                       # Current time in ISO-8601 format
-#   expiry = timeadd(timestamp(), "24h")       # Add 24 hours to the current time
+  # Define the resource types
+  resource_types {
+    service   = true
+    container = true
+    object    = true
+  }
 
-#   # Define the permissions
-#   permissions {
-#     read    = true
-#     create  = true
-#     write   = true
-#     list    = false
-#     delete  = false
-#     add     = false
-#     update  = false
-#     process = false
-#     tag     = false
-#     filter  = false
-#   }
-#   depends_on = [azurerm_storage_blob.script_blob]
-# }
+  # Define the storage account services
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  # Use dynamic time for start and expiry
+  start  = timestamp()                       # Current time in ISO-8601 format
+  expiry = timeadd(timestamp(), "24h")       # Add 24 hours to the current time
+
+  # Define the permissions
+  permissions {
+    read    = true
+    create  = true
+    write   = true
+    list    = false
+    delete  = false
+    add     = false
+    update  = false
+    process = false
+    tag     = false
+    filter  = false
+  }
+  depends_on = [azurerm_storage_blob.script_blob]
+}
 
 
 
