99947a77df3e8eb9a1fd7f3e181652ab87a9a91d
Oscar Pettersson <pson93@gmail.com>
2025-01-30 13:33:15 +0100

    push

--- modules/azure_storage_account/main.tf
+++ modules/azure_storage_account/main.tf
@@ -49,7 +49,7 @@ resource "azurerm_storage_account_network_rules" "storage_rules" {
   storage_account_id = azurerm_storage_account.blob_storage_account.id
   default_action = "Deny" 
   virtual_network_subnet_ids = values(var.subnet_ids)
-  ip_rules = [var.runner_public_ip] 
+  ip_rules = [var.runner_public_ip]
 }
 
 resource "azurerm_storage_container" "script_container" {
